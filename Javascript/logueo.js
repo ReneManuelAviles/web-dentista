4f061569baffb0e1bea69aee49d62b3099b9ac90
ReneManuelAviles <renemanuelaviles@gmail.com>
2022-06-01 20:29:20 -0400

    first commit

---
+++ Javascript/logueo.js
@@ -0,0 +1,8 @@
+let btn = document.querySelector('#btn');
+let sidebar = document.querySelector('.sidebar');
+let search = document.querySelector('#btn');
+
+btn.addEventListener('click', () => {
+    sidebar.classList.toggle('active');
+    
+});
